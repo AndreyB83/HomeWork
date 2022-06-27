5b8f11d91d69ed8597362f339cfaca329cf9f6cd
Andrey Bobrovich <andreyidei@bk.ru>
2022-06-27 15:33:03 +0300

    Add exercise

--- h_w2/strings_2.py
+++ h_w2/strings_2.py
@@ -1,1 +1,10 @@
+import  re
+
 string_with_simbol = str(input('Введите строку с символами:'))
+simbol = len(re.findall('\W+', string_with_simbol))
+if simbol:
+    print('Количество спец. символов в строке:', simbol)
+else:
+    print('Символы не обнаружены')
+
+
